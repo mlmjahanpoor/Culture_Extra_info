58f8009d359954b50d7210de094652d5baab0b6b
Moslem jahanpoor <mlmjahanpoor@gmail.com>
2022-08-18 16:58:52 +0430

    jahanpoor 16:58

--- Sample_culture/Program.cs
+++ Sample_culture/Program.cs
@@ -1,6 +1,25 @@
 ﻿using Sample_culture;
+using System.Text;
 using static Sample_culture.TestCulture;
 
-var info = TimeZoneInfo.Local.Id;
+//var info = TimeZoneInfo.Local.Id;
 
-Console.WriteLine(TestCulture.GetRequestedDateTime(DateTime.UtcNow, TimeZoneId.Arabian));
+//Console.WriteLine(TestCulture.GetRequestedDateTime(DateTime.UtcNow, TimeZoneId.Arabian));
+
+#region get all timezones
+
+var orderd = TimeZoneInfo.GetSystemTimeZones().OrderBy(x => x.Id);
+
+var sb = new StringBuilder();
+foreach (var timeZoneInfo in orderd)
+{
+    var str = $"{timeZoneInfo.StandardName} => " + TestCulture.GetRequestedDateTime(DateTime.UtcNow, timeZoneInfo.Id);
+    sb.AppendLine(str);
+    Console.WriteLine(str);
+}
+
+var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+
+File.WriteAllBytes("timeZones.txt", bytes);
+
+#endregion get all timezones
